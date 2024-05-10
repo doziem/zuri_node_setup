36306a33640e295a80207a5048f8785f7e2108fb
Kenneth Chidozie <agbokennethchidoziem@gmail.com>
2024-05-10 15:53:44 -0700

    set up http server and handle cors error

---
+++ server.js
@@ -0,0 +1,46 @@
+const http = require('node:http');
+const os = require('os');
+
+const PORT = 3000;
+   
+  const server = http.createServer((req, res) => {
+
+    if (req.method === 'OPTIONS') {
+      res.writeHead(200, {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      });
+      res.end();
+      return;
+    };
+    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    if (req.method === 'GET') {
+      setTimeout(() => {
+        //CPU and OS information
+        const cpuData = os.cpus();
+        const osData = {
+          platform: os.platform(),
+          type: os.type(),
+          release: os.release(),
+          totalMemory: os.totalmem(),
+          freeMemory: os.freemem(),
+          hostname:os.hostname(),
+          userInfo:os.userInfo()
+        };
+        
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({ cpuData, osData }));
+      }, Math.floor(Math.random() * 5000));
+    } 
+
+    // res.writeHead(200, { 'Content-Type': 'application/json' });
+    // res.end(JSON.stringify(os));
+  });
+
+  
+  server.listen(PORT, () => {
+    console.log(`Server is running on Port:${PORT}`);
+  });
